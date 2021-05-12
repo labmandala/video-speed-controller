3f316619eeec2e0b7fbe27ac43b992c27e8365a6
sarah <skg@neofilm.us>
2021-05-11 20:54:44 -0400

    Change event adjusts video speed to slider value.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    $("#speedSlider").slider({min:0, max:2, value:1, step:.01});
+    $("#speedSlider").slider({min:0, max:2, value:1, step:.01,
+        change: function(event, ui){
+           // alert(ui.value);
+           $("#myVideo")[0].playbackRate = ui.value; //playbackRate is DOM property, not jquery property
+        }
+    });
 });
